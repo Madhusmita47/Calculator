9726f713a24dc7ece09055fd046c11fba0b200d7
Madhusmita47 <madhusmita238091@gmail.com>
2024-01-06 19:16:22 +0530

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import CalculatorPractice from './CalculatorPractice'
+
+const App = () => {
+  return (
+    <div>
+    <CalculatorPractice/>
+    </div>
+  )
+}
+
+export default App
